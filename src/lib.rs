66f3f8af2fbc16c5ec3fc4035a2500fbd4a60250
Johannes Hahn <unitone@bastelbeat.de>
2021-12-16 20:17:00 +0100

    set as u32 for arm

--- src/lib.rs
+++ src/lib.rs
@@ -670,6 +670,8 @@ impl RxState {
                 let c = cache.0.as_ptr();
                 unsafe {
                     randomx_init_dataset(d, c, start.into(), count.into());
+                    #[cfg(target_arch = "arm")]
+                    randomx_init_dataset(d, c, start as u32, count as u32);
                 }
             }));
             start += count;
